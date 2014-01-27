0d9b564777b4fab05a6b14ce991a58c08c2049c6
Chien <shen0915jian@gmail.com>
2014-01-27 16:38:10 +0800

    Update HelloGit.java
    update code from github.

--- HelloGit/src/com/HelloGit.java
+++ HelloGit/src/com/HelloGit.java
@@ -4,8 +4,9 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,git.");
-		System.out.println("just update code");
+		
 		System.out.println("update local code to github.");
+		System.out.println("just update code");
 	}
 
 }
