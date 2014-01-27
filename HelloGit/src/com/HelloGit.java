e8d1239a8597d97c737d4501787dc5f3b5cf257b
shen0915jian <shen0915jian@gmail.com>
2014-01-27 16:34:14 +0800

    update code from local to github.

--- HelloGit/src/com/HelloGit.java
+++ HelloGit/src/com/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello,git.");
 		System.out.println("just update code");
+		System.out.println("update local code to github.");
 	}
 
 }
